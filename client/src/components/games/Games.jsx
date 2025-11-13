3af9253bb553edf573cfc8fdf8a445dc218ed845
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2025-11-14 00:28:57 +0200

    Show message if there are no added games yet

--- client/src/components/games/Games.jsx
+++ client/src/components/games/Games.jsx
@@ -25,14 +25,11 @@ export default function Games() {
     return (
         <section id="catalog-page">
             <h1>Catalog</h1>
-            {/* <!-- Display div: with information about every game (if any) --> */}
             <div className="catalog-container">
 
-                {games.map((game) => <Game key={game._id} {...game} />)}
+                {games.length ? games.map((game) => <Game key={game._id} {...game} />) : <h3 className="no-articles">No Added Games Yet</h3>}
 
             </div>
-            {/* <!-- Display paragraph: If there is no games  --> */}
-            {/* <!-- <h3 className="no-articles">No Added Games Yet</h3> --> */}
         </section>
     );
 }
