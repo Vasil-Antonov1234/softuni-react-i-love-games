3abf950e8b40570891e49ecb2c8e44a7676ceff1
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2025-11-13 22:58:40 +0200

    Extract header and footer into separate components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,24 +1,11 @@
+import Footer from "./footer/Footer.jsx"
+import Header from "./header/Header.jsx"
+
 function App() {
 
     return (
         <>
-            <header>
-                {/* <!-- Navigation --> */}
-                <nav>
-                    <a className="home" href="#"> <img src="./images/logo.png" alt="logo" /> </a>
-                    <a href="#">Catalog</a>
-                    {/* <!-- Logged-in users --> */}
-                    <div id="user">
-                        <a href="#">Add Game</a>
-                        <a href="#">Logout</a>
-                    </div>
-                    {/* <!-- Guest users --> */}
-                    <div id="guest">
-                        <a href="#">Login</a>
-                        <a href="#">Register</a>
-                    </div>
-                </nav>
-            </header>
+            <Header />
 
             {/* <!--Home Page--> */}
             <section id="welcome-world">
@@ -64,9 +51,7 @@ function App() {
                         </div>
 
                     </div>
-                    <footer>
-                        <p className="center-text">React Course Exercise @ SoftUni &trade;</p>
-                    </footer>
+                    <Footer />
                 </div>
             </section>
         </>
