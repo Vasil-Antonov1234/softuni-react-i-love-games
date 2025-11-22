0a8ffc77ca69843eb5b1dd2cfcf7645c77661f93
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2025-11-22 11:46:12 +0200

    Add unified approach for controlled edit form

--- client/src/components/edit/Edit.jsx
+++ client/src/components/edit/Edit.jsx
@@ -1,4 +1,24 @@
+import { useState } from "react";
+
 export default function Edit() {
+    const initialValues = {
+        title: "",
+        genre: "",
+        players: "",
+        date: "",
+        imageUrl: "",
+        summary: ""
+    }
+
+    const [values, setVasluse] = useState(initialValues)
+
+    function changeHandler(event) {
+        setVasluse(state => ({
+            ...state,
+            [event.target.name]: event.target.value
+        }))
+    }
+
     return (
         <section id="edit-page">
             <form id="add-new-game">
@@ -8,33 +28,74 @@ export default function Edit() {
 
                     <div className="form-group-half">
                         <label htmlFor="gameName">Game Name:</label>
-                        <input type="text" id="gameName" name="gameName" placeholder="Enter game title..." />
+                        <input 
+                            onChange={changeHandler}
+                            value={values.title}
+                            type="text" 
+                            id="gameName" 
+                            name="title" 
+                            placeholder="Enter game title..." 
+                        />
                     </div>
 
                     <div className="form-group-half">
                         <label htmlFor="genre">Genre:</label>
-                        <input type="text" id="genre" name="genre" placeholder="Enter game genre..." />
+                        <input 
+                            onChange={changeHandler}
+                            value={values.genre}
+                            type="text" 
+                            id="genre" 
+                            name="genre" 
+                            placeholder="Enter game genre..." 
+                        />
                     </div>
 
                     <div className="form-group-half">
                         <label htmlFor="activePlayers">Active Players:</label>
-                        <input type="number" id="activePlayers" name="activePlayers" min="0" placeholder="0" />
+                        <input 
+                            onChange={changeHandler}
+                            value={values.players}
+                            type="number" 
+                            id="activePlayers" 
+                            name="players" 
+                            min="0" 
+                            placeholder="0" 
+                        />
                     </div>
 
                     <div className="form-group-half">
                         <label htmlFor="releaseDate">Release Date:</label>
-                        <input type="date" id="releaseDate" name="releaseDate" />
+                        <input 
+                            onChange={changeHandler}
+                            value={values.date}
+                            type="date" 
+                            id="releaseDate" 
+                            name="date" 
+                        />
                     </div>
 
                     <div className="form-group-full">
                         <label htmlFor="imageUrl">Image URL:</label>
-                        <input type="text" id="imageUrl" name="imageUrl" placeholder="Enter image URL..." />
+                        <input 
+                            onChange={changeHandler}
+                            value={values.imageUrl}
+                            type="text" 
+                            id="imageUrl" 
+                            name="imageUrl" 
+                            placeholder="Enter image URL..." 
+                        />
                     </div>
 
                     <div className="form-group-full">
                         <label htmlFor="summary">Summary:</label>
-                        <textarea name="summary" id="summary" rows="5"
-                            placeholder="Write a brief summary..."></textarea>
+                        <textarea 
+                            onChange={changeHandler}
+                            value={values.summary}
+                            name="summary" 
+                            id="summary" 
+                            rows="5"
+                            placeholder="Write a brief summary...">
+                        </textarea>
                     </div>
 
                     <input className="btn submit" type="submit" value="EDIT GAME" />
