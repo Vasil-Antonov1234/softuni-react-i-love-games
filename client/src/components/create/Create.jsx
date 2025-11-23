ac1149493432f7529493e8f8101d52016665a8a5
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2025-11-23 19:21:46 +0200

    Add file upload option with preview in create game
    Updated Create component to support Firebase usage (Firebase storage is not activated yet).

--- client/src/components/create/Create.jsx
+++ client/src/components/create/Create.jsx
@@ -1,37 +1,56 @@
 import { useNavigate } from "react-router";
 import request from "../../utils/request.js";
+import { useEffect, useState } from "react";
+import { ref, getDownloadURL, uploadBytes } from "firebase/storage";
+import { storage } from "../../firebase.js";
 
 export default function Create() {
+    const [imageUpload, setImageUpload] = useState(false);
+    const [imagePreview, setImagePreview] = useState(null)
+
+    useEffect(() => {
+        setImagePreview(null);
+    }, [imageUpload])
 
     const navigate = useNavigate();
 
     async function createGameHandler(event) {
         event.preventDefault();
         const formData = new FormData(event.target);
-        const newGameData = Object.fromEntries(formData);
+
+        const { image, ...newGameData } = Object.fromEntries(formData);
+
+        if (imageUpload) {
+            const imageRef = ref(storage, `images/${image.name}`);
+            await uploadBytes(imageRef, image);
+            newGameData.imageUrl = await getDownloadURL(imageRef);
+        } else {
+            newGameData.imageUrl = image;
+        }
+
         newGameData.players = Number(newGameData.players);
         newGameData._createdOn = Date.now();
 
         try {
-            // const response = await fetch(BASE_URL, {
-            //     method: "POST",
-            //     headers: {
-            //         "content-type": "application/json"
-            //     },
-            //     body: JSON.stringify(newGameData)
-            // });
-
-            // const result = await response.json();
-
-            const result = await request("/games", "POST", {"content-type": "application/json"}, newGameData);
-
-            console.log(result);
+            await request("/games", "POST", { "content-type": "application/json" }, newGameData);
 
             navigate("/games");
         } catch (error) {
             alert(error.message)
         };
     };
+
+    function imageUploadClickHandler() {
+
+        setImageUpload(state => !state);
+    };
+
+    function imageChangeHandler(event) {
+        const image = event.target.files[0];
+        const imageUrl = URL.createObjectURL(image);
+
+        setImagePreview(imageUrl);
+    }
 
     return (
         <section id="add-page">
@@ -61,8 +80,13 @@ export default function Create() {
                     </div>
 
                     <div className="form-group-full">
-                        <label htmlFor="imageUrl">Image URL:</label>
-                        <input type="text" id="imageUrl" name="imageUrl" placeholder="Enter image URL..." />
+                        <label htmlFor="image">{imageUpload ? "Image Upload:" : "Image URL:"}</label>
+                        <button type="button" className="details-button" onClick={imageUploadClickHandler}>{imageUpload ? "Image Url" : "Image Upload"}</button>
+                        {imageUpload ?
+                            <input type="file" id="image" name="image" placeholder="Upload File" onChange={imageChangeHandler} /> :
+                            <input type="text" id="image" name="image" placeholder="Enter image URL..." />}
+
+                        {imagePreview && <img src={imagePreview} alt="Image Preview"></img>}
                     </div>
 
                     <div className="form-group-full">
---
+++ client/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJzPm4yXf_k8QSP6fjMdL3LVLP0sTrXnU",
+  authDomain: "i-love-game-8d79c.firebaseapp.com",
+  projectId: "i-love-game-8d79c",
+  storageBucket: "i-love-game-8d79c.firebasestorage.app",
+  messagingSenderId: "704968859362",
+  appId: "1:704968859362:web:992224aa5326e3654d08bb"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
